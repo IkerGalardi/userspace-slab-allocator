3c0f85df177a6f22eb3862b76eab901a32f461cd
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-01-31 09:19:59 +0100

    Freeing of cache and PAGE_SIZE define

--- slab_allocator/src/slab.c
+++ slab_allocator/src/slab.c
@@ -1,16 +1,37 @@
 #include "slab.h"
 
-#include <sys/mman.h>
 #include <stddef.h>
+#include <assert.h>
+
+#include <sys/mman.h>
+
+#define PAGE_SIZE 4 * 1024
 
 struct mem_slab* mem_slab_create(int size, int alignment) {
+    // Small checks before starting
+    assert((size > 0) && "Slab size must be bigger than 0");
+
     // TODO: move the slab struct to the end of the buffer.
     // TODO: alignment requirements are ignored right now, dont do that :(
-    struct mem_slab* result = mmap(NULL, 1024 * 4, PROT_READ | PROT_WRITE, MAP_ANON, -1, 0);
+    struct mem_slab* result = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_ANON, -1, 0);
+    result->next  = NULL;
     result->size = size;
     result->alignment = alignment;
 
 
 
     return result;
+}
+
+struct mem_slab* mem_slab_free(struct mem_slab* slab) {
+    assert((slab->ref_count > 0) && "Use after free of slab");
+
+    // Decrement the reference count and if no longer referenced free 
+    // the memory.
+    // TODO: check if slab memory should be checked or not before freeing
+    //       the page
+    slab->ref_count--;
+    if(slab->ref_count == 0) {
+        munmap((void*)slab, PAGE_SIZE);
+    }
 }
--- slab_allocator/src/slab.h
+++ slab_allocator/src/slab.h
@@ -1,13 +1,19 @@
 #ifndef _SLAB_H
 #define _SLAB_H
 
+#include <stdint.h>
+
 // TODO: add multi threaded lock stuff
 struct mem_slab {
+    struct mem_slab* next;
     int size;
     int alignment;
+    int ref_count;
+    uint8_t data[];
 };
 
 // TODO: check if constructors and destructors are needed for userspace
 struct mem_slab* mem_slab_create(int size, int alignment);
+struct mem_slab* mem_slab_free(struct mem_slab* slab);
 
 #endif // _SLAB_H
