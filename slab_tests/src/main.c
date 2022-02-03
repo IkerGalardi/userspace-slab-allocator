25d9235adfb2635470e76c4a291b503e8385c051
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-02-03 09:22:47 +0100

    Cache NULL checking

--- slab_tests/src/main.c
+++ slab_tests/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <slab.h>
 
@@ -6,6 +7,10 @@ int main(int argc, char** argv) {
     printf("Hello world!\n");
 
     struct mem_slab* slab = mem_slab_create(sizeof(int), 0);
+    if(slab == NULL) {
+        fprintf(stderr, "Error: slab creation failed!\n");
+        exit(1);
+    }
 
     void* allocated[5];
     for(int i = 0; i < 5; i++) {
