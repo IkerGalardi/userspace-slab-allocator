806c4b2353d147bc0d1bf5bdff4fffaa04d1ed77
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-03-01 09:14:59 +0100

    Added more tests

--- slab_tests/src/main.c
+++ slab_tests/src/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include <slab.h>
 
-int main(int argc, char** argv) {
-    printf("Hello world!\n");
+#define TEST_NUMBER_OF_FULL_ALLOCATIONS 100
 
+void test_basic_allocations() {
     struct mem_slab* slab = mem_slab_create(sizeof(int), 0);
     if(slab == NULL) {
         fprintf(stderr, "Error: slab creation failed!\n");
@@ -14,10 +15,69 @@ int main(int argc, char** argv) {
 
     void* allocated[5];
     for(int i = 0; i < 5; i++) {
+        printf("TEST: Allocation number %i\n", i);
         allocated[i] = mem_slab_alloc(slab);
     }
 
+    printf("TEST: Deallocating allocation number 2\n");
     mem_slab_dealloc(slab, allocated[2]);
 
-    void* a = mem_slab_alloc(slab);
+    allocated[2] = mem_slab_alloc(slab);
+
+    printf("TEST: Deallocating all\n");
+    for(int i = 0; i < 5; i++) {
+        mem_slab_dealloc(slab, allocated[i]);
+    }
+
+    printf("TEST: Destroying cache\n");
+    mem_slab_free(slab);
+}
+
+void test_allocate_deallocate_all() {
+    struct mem_slab* slab = mem_slab_create(sizeof(int), 0);
+
+    void* allocations[TEST_NUMBER_OF_FULL_ALLOCATIONS];
+
+    // Allocate a lot
+    for(int i = 0; i < TEST_NUMBER_OF_FULL_ALLOCATIONS; i++) {
+        allocations[i] = mem_slab_alloc(slab);
+    }
+
+    printf("TEST: allocated %i times\n", TEST_NUMBER_OF_FULL_ALLOCATIONS);
+    
+    // Deallocate all
+    for(int i = 0; i < TEST_NUMBER_OF_FULL_ALLOCATIONS; i++) {
+        mem_slab_dealloc(slab, allocations[i]);
+    }
+
+    printf("TEST: deallocated %i times\n", TEST_NUMBER_OF_FULL_ALLOCATIONS);
+
+    mem_slab_free(slab);
+}
+
+void test_full_cache() {
+    struct mem_slab* slab = mem_slab_create(sizeof(int), 0);
+    size_t alloc_number = 0;
+
+    while(true) {
+        void* allocation = mem_slab_alloc(slab);
+
+        if(allocation == NULL)
+            break;
+
+        alloc_number++;
+    }
+
+    printf("TEST: Allocated %i times\n", alloc_number);
+}
+
+int main(int argc, char** argv) {
+    printf("Basic allocations:\n");
+    test_basic_allocations();
+
+    printf("Allocating and deallocating all:\n");
+    test_allocate_deallocate_all();
+
+    printf("Fulling the cache:\n");
+    test_full_cache();
 }
