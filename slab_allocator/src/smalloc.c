f9e9ab4d0d42531bbc7aa1fa259f813615e6239d
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-03-07 10:30:22 +0100

    Added logging

--- slab_allocator/src/smalloc.c
+++ slab_allocator/src/smalloc.c
@@ -2,8 +2,17 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "slab.h"
+
+#define SMALLOC_CONFIG_DEBUG
+
+#ifdef SMALLOC_CONFIG_DEBUG
+    #define debug(...) fprintf(stderr, __VA_ARGS__)
+#else
+    #define debug(...)
+#endif
 
 struct mem_slab* byte_cache = NULL;
 struct mem_slab* byte4_cache = NULL;
@@ -15,17 +24,24 @@ static void* allocate_and_grow_if_necessary_from_slab(struct mem_slab* slab) {
     void* ptr = mem_slab_alloc(slab_to_allocate);
 
     while(ptr == NULL) {
+        debug("\t* Cache %p full\n", slab);
         if(slab_to_allocate->next == NULL) {
             slab_to_allocate->next = mem_slab_create(slab_to_allocate->size, slab_to_allocate->alignment);
             
             // If no more slabs can be created, simply return NULL, probably system OOM
-            if(slab_to_allocate->next == NULL)
+            if(slab_to_allocate->next == NULL) {
+                debug("\t* Could not create more caches, system probably OOM\n");
                 return NULL;
+            }
+
+            debug("\t* Needed another cache, so created!\n");
         }
 
         slab_to_allocate = slab_to_allocate->next;
         ptr = mem_slab_alloc(slab_to_allocate);
     }
+
+    debug("\t* Allocated pointer is %p\n", ptr);
 
     return ptr;
 }
@@ -50,16 +66,21 @@ void smalloc_initialize() {
 
 void* smalloc(int size) {
     assert((size <= 0));
+    debug("* Allocation of size %i\n", size);
 
     if(size == 1) {
         return allocate_and_grow_if_necessary_from_slab(byte_cache);
     } else if(size <= 4) {
+        debug("\t* Trying to use cache pool of size 4\n", size);
         return allocate_and_grow_if_necessary_from_slab(byte4_cache);
     } else if(size <= 8) {
+        debug("\t* Trying to use cache pool of size 8\n", size);
         return allocate_and_grow_if_necessary_from_slab(byte8_cache);
     } else if(size <= 16) {
+        debug("\t* Trying to use cache pool of size 16\n", size);
         return allocate_and_grow_if_necessary_from_slab(byte16_cache);
     } else {
+        debug("\t* Could not use any cache :(\n", size);
         return malloc(size);
     }
 }
