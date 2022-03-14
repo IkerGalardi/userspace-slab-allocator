02253637c9d9ba5fd1d51b62cec6f89603776888
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-03-14 09:21:38 +0100

    Implemented freeing

--- slab_allocator/src/smalloc.c
+++ slab_allocator/src/smalloc.c
@@ -3,8 +3,11 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "slab.h"
+
+#define PAGE_SIZE 1024 * 4
 
 #define SMALLOC_CONFIG_DEBUG
 
@@ -46,27 +49,42 @@ static void* allocate_and_grow_if_necessary_from_slab(struct mem_slab* slab) {
     return ptr;
 }
 
-static struct mem_slab* get_slab_pointer_is_allocated() {
+static struct mem_slab* is_ptr_allocated_in_pool(struct mem_slab* pool, void* ptr) {
+    struct mem_slab* current_slab = pool;
+    while(current_slab != NULL) {
+        void* page_of_ptr = PAGE_SIZE * (a / PAGE_SIZE);
+
+        // If the pointer is in the same page as the slab, then the pointer
+        // was allocated on that slab
+        if((void*)current_slab == page_of_ptr) {
+            return current_slab;
+        }
+    }
+
     return NULL;
 }
 
 void smalloc_initialize() {
     byte_cache = mem_slab_create(1,  0);
     assert((byte_cache   != NULL) && "Error initializing byte cache");
+    debug("SMALLOC: created cache of size 1...\n");
 
     byte4_cache = mem_slab_create(4,  0);
     assert((byte4_cache  != NULL) && "Error initializing 4 byte cache");
+    debug("SMALLOC: created cache of size 4...\n");
 
     byte8_cache = mem_slab_create(8,  0);
     assert((byte8_cache  != NULL) && "Error initializing 8 byte cache");
+    debug("SMALLOC: created cache of size 8...\n");
 
     byte16_cache = mem_slab_create(16, 0);
     assert((byte16_cache != NULL) && "Error initializing 16 byte cache");
+    debug("SMALLOC: created cache of size 16...\n");
 }
 
 void* smalloc(int size) {
-    assert((size <= 0));
-    debug("* Allocation of size %i\n", size);
+    assert((size >= 0) && "Allocation size must be bigger than 0"); 
+    debug("SMALLOC: Allocation of size %i\n", size);
 
     if(size == 1) {
         return allocate_and_grow_if_necessary_from_slab(byte_cache);
@@ -88,16 +106,34 @@ void* smalloc(int size) {
 void sfree(void* ptr) {
     assert((ptr != NULL) && "Passed pointer should be a valid pointer");
 
-    struct mem_slab* slab = get_slab_pointer_is_allocated(ptr);
-
-    // If the pointer was not allocated using a slab it means that was allocated using malloc, 
-    // then simply free and return.
-    if(slab == NULL) {
-        free(ptr);
+    // Check if the pointer was allocated on the byte slab cache
+    struct mem_slab* ptr_slab = is_ptr_allocated_in_pool(byte_cache, ptr);
+    if(ptr_slab != NULL) {
+        mem_slab_dealloc(ptr_slab, ptr);
         return;
     }
 
-    mem_slab_dealloc(slab, ptr);
+    // Check if the pointer was allocated on the 4 byte slab cache
+    ptr_slab = is_ptr_allocated_in_pool(byte4_cache, ptr);
+    if(ptr_slab != NULL) {
+        mem_slab_dealloc(ptr_slab, ptr);
+        return;
+    }
 
-    // TODO: maybe remove the slab from the slab list?
+    // Check if the pointer was allocated on the 8 byte slab cache
+    ptr_slab = is_ptr_allocated_in_pool(byte8_cache, ptr);
+    if(ptr_slab != NULL) {
+        mem_slab_dealloc(ptr_slab, ptr);
+        return;
+    }
+
+    // Check if the pointer was allocated on the 16 byte slab cache
+    ptr_slab = is_ptr_allocated_in_pool(byte16_cache, ptr);
+    if(ptr_slab != NULL) {
+        mem_slab_dealloc(ptr_slab, ptr);
+        return;
+    }
+
+    // If not allocated on caches, the pointer was allocated with malloc so just free.
+    free(ptr);
 }
