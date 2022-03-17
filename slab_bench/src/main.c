52f5c9cd5d4b3347fccd8102c9f47816e7f3bc27
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-03-17 10:01:12 +0100

    Profiling time shown

--- slab_bench/src/main.c
+++ slab_bench/src/main.c
@@ -13,20 +13,40 @@
 #define allocation_function malloc
 #define free_function       free
 #endif // USE_SMALLOC
+
+int64_t difftimespec_ns(const struct timespec after, const struct timespec before)
+{
+    return ((int64_t)after.tv_sec - (int64_t)before.tv_sec) * (int64_t)1000000000
+         + ((int64_t)after.tv_nsec - (int64_t)before.tv_nsec);
+}
+
 void do_allocations_in_loop(int count, int alloc_size) {
     void** allocations = malloc(count * sizeof(void*));
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
 
     // Do allocations
     printf("\t Allocating %i buffers... ");
     for(int i = 0; i < count; i++) {
         allocations[i] = allocation_function(alloc_size);
     }
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    int64_t nanosecs = difftimespec_ns(end, start);
+    printf("%i ns\n", nanosecs);
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
     
     // Do allocations
     printf("\t Freeing %i buffers... ");
     for(int i = count - 1; i >= 0; i--) {
         free_function(allocations[i]);
     }
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    nanosecs = difftimespec_ns(end, start);
+    printf("%i ns\n", nanosecs);
 }
 
 int main() {
