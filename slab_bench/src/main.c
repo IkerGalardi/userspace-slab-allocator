43f454b3be6b4b104957ea655e8560bf4684f092
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-03-20 13:56:07 +0100

    CSV format for benchmarks

--- slab_bench/src/main.c
+++ slab_bench/src/main.c
@@ -6,6 +6,8 @@
 #include <slab.h>
 #include <smalloc.h>
 
+#define BENCH_NUMBER_OF_ALLOCATIONS 10000
+
 typedef void*(*malloc_function)(int);
 typedef void(*free_function)(void*);
 
@@ -42,16 +44,21 @@ void do_allocations_in_loop(int count, int alloc_size, malloc_function mf, free_
     printf("%i", nanosecs);
 }
 
+static void do_and_print_benchmark_for_size(size_t size) {
+    printf("\n%i,", size);
+    do_allocations_in_loop(BENCH_NUMBER_OF_ALLOCATIONS, size, malloc, free);
+    printf(",");
+    do_allocations_in_loop(BENCH_NUMBER_OF_ALLOCATIONS, size, smalloc, sfree);
+}
+
 int main() {
     smalloc_initialize();
 
-    printf(",System malloc,System free,Slabbed malloc,Slabbed free\n");    
-    
-    // Size 4 allocations
-    printf("4,");
-    do_allocations_in_loop(10000, 4, malloc, free);
-    printf(",");
-    do_allocations_in_loop(10000, 4, smalloc, sfree);
-
-    printf("\n16,");
+    printf(",System malloc,System free,Slabbed malloc,Slabbed free");    
+    do_and_print_benchmark_for_size(4);   
+    do_and_print_benchmark_for_size(16);   
+    do_and_print_benchmark_for_size(32);   
+    do_and_print_benchmark_for_size(128);   
+    do_and_print_benchmark_for_size(512);   
+    printf("\n");
 }
