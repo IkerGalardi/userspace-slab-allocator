801d87e768066890a1ac79b2aa82f4e278d74742
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-03-28 09:48:39 +0200

    Implemented malloc and free

--- slab_allocator/src/smalloc.c
+++ slab_allocator/src/smalloc.c
@@ -71,7 +71,7 @@ static struct mem_slab* is_ptr_allocated_in_pool(struct mem_slab* pool, void* pt
 
 void smalloc_initialize() {
     // NOTE: editing this array will change the cache configuration of smalloc
-    size_t cache_sizes[SMALLOC_CACHE_COUNT] = { 8, 16, 24, 32 };
+    size_t cache_sizes[SMALLOC_CACHE_COUNT] = { 1, 4, 8, 16 };
 
     // Initialize the caches using the configuration
     debug("SMALLOC: creating %i caches at initialization\n", SMALLOC_CACHE_COUNT);
@@ -86,8 +86,38 @@ void smalloc_initialize() {
 void* smalloc(size_t size) {
     assert((size >= 0) && "Allocation size must be bigger than 0"); 
     debug("SMALLOC: Allocation of size %i\n", size);
+
+    // Find a suitable cache and try to allocate on it.
+    for(int i = 0; i < SMALLOC_CACHE_COUNT; i++) {
+        struct mem_slab* slab = caches[i];
+
+        debug("\t* Trying to allocate on cache of size %i\n", slab->size);
+
+        // NOTE: Assumes that the chache configuration sizes are sorted.
+        if(size <= slab->size) {
+            return allocate_and_grow_if_necessary_from_slab(slab);
+        }
+    }
+
+    // If this point is reached, means that no cache is suitable for allocating the
+    // given size.
+    return malloc(size);
 }
 
 void sfree(void* ptr) {
     assert((ptr != NULL) && "Passed pointer should be a valid pointer");
+
+    for(int i = 0; i < SMALLOC_CACHE_COUNT; i++) {
+        struct mem_slab* slab = is_ptr_allocated_in_pool(caches[i], ptr);
+
+        // If the returned slab pointer is not NULL the pointer was allocated there.
+        if(slab != NULL) {
+            mem_slab_dealloc(slab, ptr);
+            return;
+        }
+    }
+
+    // If this point is reached, means that the pointer was not allocated on any
+    // cache. 
+    free(ptr);
 }
