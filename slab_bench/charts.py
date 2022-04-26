5ff5d6737488881119b8c67be89fc47d2a1ebae0
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-04-26 10:25:13 +0200

    Updated charting script to support double execution benchmarks

--- slab_bench/charts.py
+++ slab_bench/charts.py
@@ -2,12 +2,11 @@ import csv
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import itertools
 
 CSV_ALLOC_SIZE = 0
-CSV_SYS_MALLOC = 1
-CSV_SYS_FREE = 2
-CSV_SLAB_MALLOC = 3
-CSV_SLAB_FREE = 4
+CSV_MALLOC = 1
+CSV_FREE = 2
 
 OWN_ALLOC_SIZE = 0
 OWN_SYS_TIME = 1
@@ -74,34 +73,35 @@ def plot_speed_comparisons(data):
     plt.show()
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print("ERROR: incorrect command line arguments")
-        print("Usage: ./charts.py <benchmark_file>")
+        print("Usage: ./charts.py <system_benchmark> <slab_benchmark>")
 
     malloc_data = []
     free_data = []
 
     # Read the data and process a table that looks like this:
     # [ alloc_size, system_malloc_time, slabbed_malloc_time, af ]
-    with open(sys.argv[1]) as csv_file:
-        csv_reader = csv.reader(csv_file)
-        header = next(csv_reader)
-
-        for row in csv_reader:
-            # Read the data from the CSV file
-            alloc_size = row[CSV_ALLOC_SIZE]
-            system_malloc_time = int(row[CSV_SYS_MALLOC])
-            system_free_time = int(row[CSV_SYS_FREE])
-            slabbed_malloc_time = int(row[CSV_SLAB_MALLOC])
-            slabbed_free_time = int(row[CSV_SLAB_FREE])
-
-            # Calculate acceleration factor (af > 1 is an improvement, else slower)
-            malloc_af = system_malloc_time / slabbed_malloc_time
-            free_af = system_free_time / slabbed_free_time
-
-            # Append to the global data
-            malloc_data.append([ alloc_size, system_malloc_time, slabbed_malloc_time, malloc_af ])
-            free_data.append([ alloc_size, system_free_time, slabbed_free_time, free_af ])
+    system_csv = csv.reader(open(sys.argv[1]))
+    slab_csv =   csv.reader(open(sys.argv[2]))
+    system_header = next(system_csv)
+    slab_header = next(slab_csv)
+ 
+    for (system_row, slab_row) in zip(system_csv, slab_csv):
+        # Read the data from the CSV file
+        alloc_size = system_row[CSV_ALLOC_SIZE]
+        system_malloc_time = int(system_row[CSV_MALLOC])
+        system_free_time =   int(system_row[CSV_FREE])
+        slabbed_malloc_time = int(slab_row[CSV_MALLOC])
+        slabbed_free_time =   int(slab_row[CSV_FREE])
+ 
+        # Calculate acceleration factor (af > 1 is an improvement, else slower)
+        malloc_af = system_malloc_time / slabbed_malloc_time
+        free_af = system_free_time / slabbed_free_time
+ 
+        # Append to the global data
+        malloc_data.append([ alloc_size, system_malloc_time, slabbed_malloc_time, malloc_af ])
+        free_data.append([ alloc_size, system_free_time, slabbed_free_time, free_af ])
 
 
     # Plot the AF per size
