12fade0e5791681eef5e139149b0770e4efde0aa
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-05-18 10:08:57 +0200

    Using beta distribution to choose sizes

--- slab_bench/randomsize_fifo_synthetic.c
+++ slab_bench/randomsize_fifo_synthetic.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 
 #include <smalloc.h>
+#include <math.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
 
 #ifdef USE_SMALLOC
     #define allocate(x)   smalloc(x)
@@ -11,20 +14,27 @@
     #define deallocate(x) free(x)
 #endif // USE_SMALLOC
 
-#define ITERATION_COUNT 1000
+#define ITERATION_COUNT 1
 #define ALLOCATION_COUNT 100000
+
+#define MIN_ALLOCATION_SIZE 1
+#define MAX_ALLOCATION_SIZE 800
+
+#define ALPHA 2
+#define BETA  20
 
 int main() {
     smalloc_initialize();
 
-    int allocation_sizes[] = { 2, 8, 12, 32, 37, 128, 512 };
+    gsl_rng* r = gsl_rng_alloc(gsl_rng_default);
+    gsl_rng_set(r, 0);
 
     void** allocation = malloc(ALLOCATION_COUNT * sizeof(void*));
 
     for(int it = 0; it < ITERATION_COUNT; it++) {
         for(int i = 0; i < ALLOCATION_COUNT; i++) {
-            int index = rand() % 7;
-            allocation[i] = allocate(allocation_sizes[index]); 
+            int allocation_size = roundf(MAX_ALLOCATION_SIZE * gsl_ran_beta(r, ALPHA, BETA));
+            allocation[i] = allocate(allocation_size); 
         }
 
         for(int i = 0; i < ALLOCATION_COUNT; i++) {
