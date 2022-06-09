7fb2dc7b1daad901be013fe204384f2db0183642
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-06-09 09:39:29 +0200

    Cfrac now builds

--- bench/bench.sh
+++ bench/bench.sh
@@ -11,11 +11,19 @@ gcc bench/randomsize_fifo_synthetic.c -o bin/randomsize_fifo_synthetic $FLAGS ||
 echo "  [+] randomsize_fifo_synthetic_smalloc"
 gcc bench/randomsize_fifo_synthetic.c -o bin/randomsize_fifo_synthetic_smalloc $FLAGS -DUSE_SMALLOC || exit
 
-echo "  [+] dash"
+echo "  [+] cfrac"
+cd bench/cfrac
+make -f oldMakefile clean
+make -f oldMakefile
+mv cfrac ../../bin/cfrac_sys
+make -f oldMakefile clean
+make -f oldMakefile ADDITIONAL_FLAGS=-DUSE_SMALLOC
+mv cfrac ../../bin/cfrac_smalloc
+cd ..
 
 echo "RANDOM SIZE, FIFO FREE, SYNTHETIC"
 echo "  · glibc allocator"
-./bin/randomsize_fifo_synthetic || exit
+nice -n -20 ./bin/randomsize_fifo_synthetic || exit
 
 echo "  · slabed allocator"
-./bin/randomsize_fifo_synthetic_smalloc || exit
+nice -n -20 ./bin/randomsize_fifo_synthetic_smalloc || exit
