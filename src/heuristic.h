42bebaa2912610c91bef3c57e28c9ec4ed7b188e
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-06-27 09:25:59 +0200

    Documentation for heuristic functions

--- src/heuristic.h
+++ src/heuristic.h
@@ -20,8 +20,14 @@ struct heuristic_data {
     size_t shrink_count;
 };
 
+/*
+ * Returns how much the slab pool should grow given the parameters and slab pool statistics.
+ */
 int heuristic_decision_grow_count(struct heuristic_params params, struct heuristic_data data);
 
+/*
+ * Returns whether a slab should be removed or not based on the given parameters and pool statistics
+ */
 bool heuristic_decision_does_unmap(struct heuristic_params params, struct heuristic_data data);
 
 #endif // SLAB_HEURISTIC_H
